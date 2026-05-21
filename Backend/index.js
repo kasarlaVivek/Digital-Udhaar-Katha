832c12df41d6acfda3ee6f9cbca942c548eb1169
kasarlaVivek <vivekkasarla781@gmail.com>
2026-05-21 10:29:49 +0530

    deployement updates

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176", "https://digital-udhaar-katha.vercel.app"],
   credentials: true
 }));
 app.use(bodyParser.json());
